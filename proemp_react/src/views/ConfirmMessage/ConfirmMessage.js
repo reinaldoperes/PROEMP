38658e6a813b7f3fbfd47d38d6daf3cc2d49f1e7
Matheus Ferreira de Favari <matheus.f.favari@gmail.com>
2020-05-19 11:31:54 -0300

    .

--- proemp_react/src/views/ConfirmMessage/ConfirmMessage.js
+++ proemp_react/src/views/ConfirmMessage/ConfirmMessage.js
@@ -1,22 +1,10 @@
 import React from "react";
 import ButtonCustom from "components/CustomButtons/Button.js";
-import { makeStyles } from "@material-ui/core/styles";
 import { Redirect } from "react-router-dom";
-
-const useStyles = makeStyles(theme => ({
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}));
 
 const ConfirmMessage = ({ ...props }) => {
   const [showLogin, setShowlogin] = React.useState(false);
   const [msg, setMsg] = React.useState(false);
-  const classes = useStyles();
   const handleSubmit = () => {
     setShowlogin(true);
   };
@@ -39,13 +27,12 @@ const ConfirmMessage = ({ ...props }) => {
   return (
     <div>
       <h6>{msg}</h6>
-      <form className={classes.form} onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <ButtonCustom
           type="submit"
           fullWidth
           variant="contained"
           color="warning"
-          className={classes.submit}
         >
           Voltar
         </ButtonCustom>
